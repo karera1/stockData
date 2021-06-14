201b2c3e58bfbbcf587683867d33a1482a86de72
Deepak <karera.deepak@gmail.com>
2021-06-14 03:44:07 -0400

    Correcting Issues

--- src/main/java/com/yahoofinance/stock/stockData/StockDataApplication.java
+++ src/main/java/com/yahoofinance/stock/stockData/StockDataApplication.java
@@ -2,8 +2,11 @@ package com.yahoofinance.stock.stockData;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
+@ComponentScan({"com.yahoofinance.stock.stockData.controller"})
 public class StockDataApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/yahoofinance/stock/stockData/controller/stockController.java
+++ src/main/java/com/yahoofinance/stock/stockData/controller/stockController.java
@@ -3,6 +3,9 @@ package com.yahoofinance.stock.stockData.controller;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -15,7 +18,7 @@ import yahoofinance.YahooFinance;
 @Controller
 public class stockController {
 
-	private Map<String, String> stocks = new HashMap<String, String>();
+	private Map<String, String> stocks = new ConcurrentHashMap <String, String>();
 
 	@Autowired
 	SimpMessagingTemplate template;
@@ -23,24 +26,38 @@ public class stockController {
 	@MessageMapping("/stock.addStock")
 	@SendTo("/topic/public")
 	public void addStock(@Payload Map<String, String> stockobj) throws Exception {
-		
+
 		String stockName = stockobj.get("key");
 		try {
-		String str = YahooFinance.get(stockobj.get("key")).toString();
-		if(null!=str) {
-		
-		stocks.put(stockName, str);
-		template.convertAndSend("/topic/public", stocks);
-		}
-		System.out.println("Sent" + stocks);}
-		catch(Exception e) {
-			System.out.println("stockid :"+stockobj.get("key")+"is not a valid entry");
-			String str="Exception";
+			String str = YahooFinance.get(stockobj.get("key")).toString();
+			if (null != str) {
+
+				stocks.put(stockName, str);
+				template.convertAndSend("/topic/public", stocks);
+			}
+		} catch (Exception e) {
+			System.out.println("stockid :" + stockobj.get("key") + " is not a valid entry");
+			String str = "Exception";
 			stocks.put(stockName, str);
 			template.convertAndSend("/topic/public", stocks);
 		}
+		System.out.println("Sent" + stocks);
+		while (!stocks.isEmpty()) {
+			for (Map.Entry<String, String> entry : stocks.entrySet()) {
+				if (entry.getValue() == "Exception") {
+					stocks.remove(entry.getKey());
+				} else {
+					String ustockname = entry.getKey().toString();
+					String ustr = YahooFinance.get(ustockname).toString();
+					stocks.put(ustockname, ustr);
+					TimeUnit.SECONDS.sleep(2);
+				}
+			}
+			template.convertAndSend("/topic/public", stocks);
+			System.out.println("Sent" + stocks);
+		}
 	}
-	
+
 	@MessageMapping("/stock.removeStock")
 	@SendTo("/topic/public")
 	public void removeStock(@Payload Map<String, String> stockobj) throws IOException {
@@ -48,7 +65,6 @@ public class stockController {
 		String stockName = stockobj.get("key");
 		stocks.remove(stockName);
 		template.convertAndSend("/topic/public", stocks);
-		System.out.println("Deleted");
 
 	}
 
